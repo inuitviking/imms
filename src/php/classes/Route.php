4234789ae4a18a26b66d6cbdc7f2a5fa3fefb4f1
Angus <angus@pop-os.localdomain>
2022-04-01 23:40:47 +0200

    Tried some scssphp stuff

--- src/php/classes/Route.php
+++ src/php/classes/Route.php
@@ -3,6 +3,7 @@
 namespace Angus\Ivmdcms\php\classes;
 require_once '../vendor/autoload.php';
 use League\CommonMark\CommonMarkConverter;
+use ScssPhp\ScssPhp\Compiler;
 
 class Route {
 	/**
@@ -34,23 +35,34 @@ class Route {
 		// Fetch current path
 		$this->path				= ltrim($_SERVER['REQUEST_URI'],'/');	// Current path
 
-		$urlParams = ltrim(substr($this->path, strpos($this->path, '?')), '?'); // Get the parameters
-		$urlParams = explode('&', $urlParams); // Split them up if there are more than one
+		$urlParams				= ltrim(substr($this->path, strpos($this->path, '?')), '?'); // Get the parameters
+		$urlParams				= explode('&', $urlParams); // Split them up if there are more than one
 		// If path has "?" remove it from the path, as it will be read wrong.
 		if (str_contains($this->path, "?")) {
 			$this->path = substr($this->path, 0, strpos($this->path, "?"));
 		}
+
+		// Split the path into various elements.
 		$this->elements			= preg_split('/ [\/|?|&] /', $this->path);
+		// Check if path exists.
 		$this->pathExists = file_exists('../src/documents/' . $this->path);
+
+		// Compile SCSS
+		$compiler = new Compiler();
+		$compiler->SetImportPaths('../src/scss/');
+		$css = $compiler->compileString(file_get_contents('../src/scss/main.scss'))->getCss();
+		file_put_contents('assets/css/main.css', $css);
 
 		// Get header
 		require_once 'assets/viewables/header.php';
 
+		// Prepare the markdown converter
 		$converter = new CommonMarkConverter([
 			'html_input' => 'strip',
 			'allow_unsafe_links' => false,
 		]);
 
+		// routing.
 		if(empty($this->elements[0])){
 			echo $converter->convert(file_get_contents($this->defaultView));
 		}else{
--- web/assets/viewables/header.php
+++ web/assets/viewables/header.php
@@ -1,1 +1,14 @@
-<?php
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css" />
+	</head>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="/">Index</a></li>
+			</ul>
+		</nav>
+	</header>
+<div></div>
